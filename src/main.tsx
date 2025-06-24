3335c4db8783416702f59de5e3d93cc9b912ab6a
mayander30 <mayander30@gmail.com>
2025-06-23 23:19:16 -0300

    adding fixes for deploy in github

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,16 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { Toaster } from 'react-hot-toast';
-
 import { AuthContextProvider } from './contexts/AuthContext';
 import { queryClient } from '~/core/queryClient.ts';
-import { Routes } from '~/routes';
 import '~/index.css';
+import { Root } from './pages/Root';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <AuthContextProvider>
-        <Routes />
+        <Root />
       </AuthContextProvider>
     </QueryClientProvider>
     <Toaster />
--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,5 @@ export default defineConfig({
       '~': '/src',
     },
   },
+  base: '/space-image-builder/',
 });
